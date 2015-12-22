c4c0ce448757922bdd84197f20317bbacce12ed1
Jose A <newtsukisan@gmail.com>
2015-12-22 17:56:20 +0100

    Basic functional

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,44 @@
 module Lib
-    ( someFunc
-    ) where
+ where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+type Soduku  = [Int]
+type Row     = Int
+type Col     = Int
+type Index   = Int
+type Rows    = [Int]
+type Cols    = [Int] 
+{-La idea es crear una estructura que nos permita resolver un soduko.
+Cada uno de las filas y de las columnas tiene que contener solo una vez cada una de las cifras.
+-} 
+
+-- is a solution when all ciphers in columns and in rows are diferents 
+-- is a solution when all ciphers are diferents of zero, (no empty values)
+isSolution :: Soduku -> Bool
+isSolution sdk = noempty && alldiferents
+    where noempty      = (length $ filter (==0) sdk) == 0
+          alldiferents = True
+
+-- Obtain col and row index from index of list
+getRindex :: Index -> Row
+getRindex index  = index `div` 9
+
+getCindex :: Index -> Col
+getCindex index  = index `rem` 9
+
+getIndex :: Row -> Col -> Index
+getIndex r c = 9 * r + c
+
+--Obtain one element by row and column
+--getElement :: Soduku -> Row -> Col -> Integer
+getElement sdk row col = sdk !! (getIndex row col)
+
+-- is need to obtain rows and columns 
+-- rows dividing by 9 
+getRows :: Soduku -> [Rows]
+getRows sdk = [] 
+
+
+getCol  :: Soduku -> [Cols] 
+getCol sdk = []
--- test/Spec.hs
+++ test/Spec.hs
@@ -6,6 +6,9 @@ import Test.QuickCheck
  
 import Test.Framework.Providers.HUnit(testCase)
 import Test.HUnit
+import Lib          -- Functions
+
+import Datatest     -- data for testing
 
 -- definicion de prueba
 prueba :: Integer -> Integer
@@ -13,11 +16,45 @@ prueba n = n + 1
 
 tests = [
         testGroup "Testing implementations for exercises" 
-           [ testCase "prueba con 1" $ prueba 1 @?= 2
-           , testCase "prueba con 2" $ prueba 2 @?= 3
-           , testProperty "Prueba de propiedad sencilla" prop1
+           [ testCase "prueba con 1" $ prueba 1 @?= 2                -- some notation
+           , testCase "prueba con 2" test_2
+           , testCase "Solucion real"          test_isSolution_1     -- must be true
+           , testCase "Se repiten una cifra"   test_isSolution_2     -- must be false
+           , testCase "Tiene un cero"          test_isSolution_3     -- must be false
+           , testCase "Counting well" $ length vacio @?= 81          -- verification 
+           , testCase "First element" $ vacio !!  0 @?=  0           -- verification
+           , testCase "one element" $ sol_bd1 !!  8 @?=  5           -- verification
+           , testCase "one element" $ sol_bd1 !! 80 @?=  6           -- verification  
+           , testCase "filas" $ getRindex  0 @?= 0                   -- must be zero 
+           , testCase "filas" $ getRindex 80 @?= 8                   -- must be zero 
+           , testCase "filas" $ getRindex 13 @?= 1                   -- must be zero 
+           , testCase "columnas" $ getCindex  0 @?= 0                -- must be zero
+           , testCase "columnas" $ getCindex 13 @?= 4                -- zero must be accounting 
+           , testCase "obtener index" $ getIndex 1 4 @?= 13          -- zero must be accounting 
+           , testCase "obtener index" $ getIndex 0 4 @?=  4          -- zero must be accounting
+           , testCase "obtener elementos" test_getElement_0          -- zero must be accounting  
+           , testCase "obtener elementos" test_getElement_1          -- zero must be accounting
+           , testCase "obtener elementos" test_getElement_2          -- zero must be accounting              
+           , testProperty "Reciprocidad" prop_testing_Index
            ]  
         ]                
+-- Caso de prueba para ver si detecta las soluciones.
+test_isSolution_1 = isSolution sol_bd1 @?=     True
+test_isSolution_2 = isSolution no_sol_bd1 @?=  False 
+test_isSolution_3 = isSolution no_sol2_bd1 @?= False
+test_getElement_0 = getElement sol_bd1 0 0 @?= 2
+test_getElement_1 = getElement sol_bd1 8 8 @?= 6 
+test_getElement_2 = getElement sol_bd1 1 4 @?= 2 
+
+-- propiedades 
+prop_testing_Index n = 
+    not (n < 0) ==>      -- valores no negativos
+     getIndex (getRindex n) (getCindex n) == n
+       where types = (n::Int)   
+
+
+
+
 
 
 -- propiedad de prueba
@@ -27,4 +64,7 @@ prop1 n = prueba n == n + 1
 test_1 = prueba 1 @?= 2
 test_2 = prueba 1 @?= 2 
 main :: IO ()
-main = defaultMain tests                                                                                 
+main = defaultMain tests
+
+
+                                                                     
